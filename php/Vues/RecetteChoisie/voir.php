5585e9a22b7c57d271aa071bef99d2f043aed4f9
Léo <leo.veysseyre@etu.univ-amu.fr>
2023-01-26 23:16:55 +0100

    ajout bouton + ajout photo de profil

--- php/Vues/RecetteChoisie/voir.php
+++ php/Vues/RecetteChoisie/voir.php
@@ -1,8 +1,8 @@
 <?php
 $test = new RecetteChoisie();
 $recup = $_GET['id_recette'];
-$ty = $test->afficherCommentaires($recup);
-$number_of_rows = count($ty);
+$comm = $test->afficherCommentaires($recup);
+$number_of_rows = count($comm);
 $resultats = $A_vue['RecetteChoisie'];
 $image = base64_encode($resultats['image']);
 
@@ -20,74 +20,82 @@ $image = base64_encode($resultats['image']);
 
 <body>
 
+    <div class="boutton">
+        <a href="/php/index.php?url=Recette" class="boutton-page-recette"><button>Retour à la page des recettes</button></a>
+    </div>
 
-<div class="container">
-    <h4><?php print_r($resultats['nom']) ?></h4>
+    <div class="container">
+        <h4><?php print_r($resultats['nom']) ?></h4>
 
-    <div class="corps">
-        <div class="avis">
-            <div class="rating">
-                <!----><a href="#5" title="Donner 5 étoiles">☆</a>
-                <!----><a href="#4" title="Donner 4 étoiles">☆</a>
-                <!----><a href="#3" title="Donner 3 étoiles">☆</a>
-                <!----><a href="#2" title="Donner 2 étoiles">☆</a>
-                <!----><a href="#1" title="Donner 1 étoile">☆</a>
+        <div class="corps">
+            <div class="avis">
+                <div class="rating">
+                    <!----><a href="#5" title="Donner 5 étoiles">☆</a>
+                    <!----><a href="#4" title="Donner 4 étoiles">☆</a>
+                    <!----><a href="#3" title="Donner 3 étoiles">☆</a>
+                    <!----><a href="#2" title="Donner 2 étoiles">☆</a>
+                    <!----><a href="#1" title="Donner 1 étoile">☆</a>
+                </div>
+                <div class="cmb_personnes">
+                    6 personnes
+                </div>
             </div>
-            <div class="cmb_personnes">
-                6 personnes
+            <div class="box_image">
+                <?php
+                        $image = base64_encode($resultats['image']);
+                        echo '<img src="data:image/png;base64,' . $image . '"class="card-img-top" alt= ' . $resultats['nom'] . '>';
+                ?>
+            </div>
+            <div class="temps_total_preparation">
+                <p>temps cuisson : <br><?php print_r($resultats['tpscuiss']) ?> min</p>
+                <p>temps repos : <br><?php print_r($resultats['tpsrepos']) ?> min</p>
+                <p>temps preparation : <br><?php print_r($resultats['tpsprepar']) ?> min</p>
+                <p>temps total : <br><?php print_r($resultats['tpstotal']) ?> min</p>
             </div>
         </div>
-        <div class="box_image">
-            <img src="data:image/png;base64,'<?php echo $image?>'id="image">
+        <div class="corps_étape">
+            <h1>Liste ingrédients : <?php print_r($resultats['ingredients']) ?></h1>
         </div>
-        <div class="temps_total_preparation">
-            <p>temps cuisson : <br><?php print_r($resultats['tpscuiss']) ?> min</p>
-            <p>temps repos : <br><?php print_r($resultats['tpsrepos']) ?> min</p>
-            <p>temps preparation : <br><?php print_r($resultats['tpsprepar']) ?> min</p>
-            <p>temps total : <br><?php print_r($resultats['tpstotal']) ?> min</p>
+        <!---divison commentaire--->
+        <div id="Commentaires">
+            Commentaires (<?php print_r($number_of_rows) ?> avis)
+            <div id="ligneComm">
+            </div>
         </div>
-    </div>
-    <div class="corps_étape">
-        <h1>Liste ingrédients : <?php print_r($resultats['ingredients']) ?></h1>
-    </div>
-    <!---divison commentaire--->
-    <div id="Commentaires">
-        Commentaires (<?php print_r($number_of_rows) ?> avis)
-        <div id="ligneComm">
+        <!--espace Commentaires-->
+        <div id="CommentairesBox">
+            <form method="get" action="../php/index.php?url=RecetteChoisie/insererComm" id="form">
+                <textarea type="textarea" id="commentInput" name="commentField"
+                    placeholder="Ajouter un avis..."></textarea>
+                <input type="hidden" name="idRecette" value="<?php echo $recup ?>">
+                <button type="submit" id="submitComment">Ajouter</button>
+            </form>
         </div>
-    </div>
-    <!--espace Commentaires-->
-    <div id="CommentairesBox">
-        <form method="get" action="../php/index.php?url=RecetteChoisie/insererComm" id ="form" >
-            <textarea type="textarea" id="commentInput" name="commentField" placeholder="Ajouter un avis..."></textarea>
-            <input type="hidden" name="idRecette" value="<?php echo $recup ?>">
-            <button type="submit" id="submitComment">Ajouter</button>
-        </form>
-    </div>
-    <ul id="comments">
-        <?php
+        <ul id="comments">
+            <?php
         for ($x = 0; $x < $number_of_rows; ++$x) {
-            $image = base64_encode($ty[$x]['photo']);
+            $image = base64_encode($comm[$x]['photo']);
             echo
                 '<li class="commentArea">' .
                 '<div class="profile">' .
-                '<img src="data:image/png;base64,' . $image . 'class="profilePicture">'.
-                '<p class="profileName">' . $ty[$x]['pseudo'] . '</p>' .
+                '<img src="data:image/png;base64,' . $image . '" class="profilePicture">'.
+                '<p class="profileName">' . $comm[$x]['pseudo'] . '</p>' .
                 '</div>' .
                 '<div class="contenuComm">' .
-                '<p class = "commentsNote">' . $ty[$x]['note'] . ' étoiles' . '</p>' .
-                '<p class = "commentsDate">' . $ty[$x]['date'] . '</p>' .
-                '<p class = "commentsTexts">' . $ty[$x]['libelle'] . '</p>' .
+                '<p class = "commentsNote">' . $comm[$x]['note'] . ' étoiles' . '</p>' .
+                '<p class = "commentsDate">' . $comm[$x]['date'] . '</p>' .
+                '<p class = "commentsTexts">' . $comm[$x]['libelle'] . '</p>' .
                 '</div>' .
                 '</li>';
         }
         ?>
-</div>
-</ul>
-</div>
+    </div>
+    </ul>
+    </div>
 
 </body>
+
 </html>
 <style>
-    @import url("/php/css/style.css");
+@import url("/php/css/style.css");
 </style>
